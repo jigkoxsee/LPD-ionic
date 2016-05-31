40070e7fa58fa29b004ede0ca1d615cbe9ef2a3f
jigkoxsee <supakorn@jigko.net>
2016-05-31 21:38:02 +0700

    add map to app

--- app/pages/tabs/tabs.js
+++ app/pages/tabs/tabs.js
@@ -1,7 +1,8 @@
 import {Page} from 'ionic-angular';
 import {Page1} from '../page1/page1';
 import {Page2} from '../page2/page2';
-import {Page3} from '../page3/page3';
+//import {Page3} from '../page3/page3';
+import {Trip} from '../trip/trip';
 
 
 @Page({
@@ -13,6 +14,6 @@ export class TabsPage {
     // should be each tab's root Page
     this.tab1Root = Page1;
     this.tab2Root = Page2;
-    this.tab3Root = Page3;
+    this.tab3Root = Trip;
   }
 }
